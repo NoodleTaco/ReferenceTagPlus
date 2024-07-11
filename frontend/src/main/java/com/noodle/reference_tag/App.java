dffba04e63a2dcde44cfd126eef74e80dc8aff7a
Donald Yubeaton <nasusneverna@gmail.com>
2024-07-11 00:35:26 -0400

    Ported over some dependencies and renamed groupid and artifacts(notably still missing fx dependencies in frontend pom)

---
+++ frontend/src/main/java/com/noodle/reference_tag/App.java
@@ -0,0 +1,10 @@
+package com.noodle.reference_tag;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
